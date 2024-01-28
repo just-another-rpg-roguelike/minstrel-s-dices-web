d8c49a8d9b6c85bec5bf1ae184cb50a229f54abc
Luis Miguel Arroyave <luismiguel.arroyave@gosite.com>
2024-01-28 10:35:05 -0500

    magic flat json map store data, kill me pls

--- src/utils/get_next_events.ts
+++ src/utils/get_next_events.ts
@@ -3,66 +3,74 @@ export const getNextEvents = (currentEvent: number): number[] => {
 };
 
 export function pickRandom<E>(arr: E[]): E {
-  return arr[Math.floor(Math.random() * arr.length)]
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export function pickRandomAndIndex<E>(arr: E[]): { index: number, value: E } {
-  const rindex = Math.floor(Math.random() * arr.length)
-  return { index: rindex, value: arr[rindex] }
+export function pickRandomAndIndex<E>(arr: E[]): { index: number; event: E } {
+  const rindex = Math.floor(Math.random() * arr.length);
+  return { index: rindex, event: arr[rindex] };
 }
 type choice = {
-  "description": string,
-  "healt": number[],
-  "charisma": number[],
-  "karma": number[],
-  "affinity": number
-}
+  description: string;
+  healt: number[];
+  charisma: number[];
+  karma: number[];
+  affinity: number;
+};
 
 type npc = {
-  "name": string,
-  "description": string,
-  "image_path": string
-  "initial_affinity": number,
-  "events": {
-    "image_path": string
-    "message": string
-    "choices": choice[]
-  }[]
+  name: string;
+  description: string;
+  image_path: string;
+  initial_affinity: number;
+  events: {
+    image_path: string;
+    message: string;
+    choices: choice[];
+  }[];
 };
 
 type eventRich = {
-  "npcKey": string
-  "npcDesciption": string
-  "npcImage": string
-  "eventImage": string
-  "eventMessage": string
-  "choices": choice[]
-}
+  npcKey: string;
+  npcDescription: string;
+  npcImage: string;
+  eventImage: string;
+  eventMessage: string;
+  choices: choice[];
+};
 
 function npcToRichEvents(npc: npc): eventRich[] {
-  const { 
-    "name": npcKey,
-    "description": npcDesciption,
-    "image_path": npcImage,
+  const {
+    name: npcKey,
+    description: npcDescription,
+    image_path: npcImage,
   } = npc;
   return npc.events.map((event) => {
-    const { "image_path": eventImage, "message": eventMessage, "choices": choices } = event;
+    const {
+      image_path: eventImage,
+      message: eventMessage,
+      choices: choices,
+    } = event;
     return {
       npcKey,
-      npcDesciption,
+      npcDescription,
       npcImage,
       eventImage,
       eventMessage,
-      choices
-    }
-  })
+      choices,
+    };
+  });
 }
 
-function getNext(eventRich) {
-  const [ index, event ] = 
+function getNext(eventRich: eventRich[]) {
+  const { index, event } = pickRandomAndIndex(eventRich);
+  return {
+    eventTree: eventRich.filter((_, i) => i !== index),
+    event,
+  };
 }
 
 const npcsToRichEvents = (npcs: npc[]): eventRich[] => {
-  return npcs.map(npcToRichEvents).flat()
-}
+  return npcs.map(npcToRichEvents).flat();
+};
 export { npcsToRichEvents };
